6017ac123d02c2b706d9b3ae6cddb41b8193494f
Trisha Bhatta <trishabhatta14@gmail.com>
2025-04-02 13:23:52 +0545

    Update task7.js
    changed the style to correct form

--- task7.js
+++ task7.js
@@ -1,11 +1,19 @@
-document.write("<table border='1' width='300'>");
+document.write("<table width='300' style='border-collapse:collapse; text-align:center; border:2px solid white;'>");
 
-for (let i = 1; i <= 5; i++) {
+// Table header with blue background, white text, and white border
+document.write("<tr style='background-color:blue; color:white; font-weight:bold; border:2px solid white;'>");
+document.write("<th style='border:2px solid white;'>SN</th>");
+document.write("<th style='border:2px solid white;'>Name</th>");
+document.write("<th style='border:2px solid white;'>Image</th>");
+document.write("</tr>");
+
+// Table rows with black text and white borders
+for (let i = 1; i <= 4; i++) {
     let bgColor = i % 2 === 0 ? "#f2f2f2" : "#ffffff"; // Alternate row colors
-    document.write(`<tr style='background-color:${bgColor}'>`);
-    for (let j = 1; j <= 3; j++) {
-        document.write(`<td>Row ${i} Col ${j}</td>`);
-    }
+    document.write(`<tr style='background-color:${bgColor}; color:black; border:2px solid white;'>`);
+    document.write(`<td style='border:2px solid white;'>${i}</td>`);
+    document.write(`<td style='border:2px solid white;'>Name${i}</td>`);
+    document.write(`<td style='border:2px solid white;'>${i}.jpg</td>`);
     document.write("</tr>");
 }
 
