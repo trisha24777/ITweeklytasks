2c5227a59c5fb0c506f79a1e0dd9d8efa59f450e
Trisha Bhatta <trishabhatta14@gmail.com>
2025-04-03 22:18:07 +0545

    Update task7.js

--- week10/task7.js
+++ week10/task7.js
@@ -1,20 +1,26 @@
-document.write("<table width='300' style='border-collapse:collapse; text-align:center; border:2px solid white;'>");
+document.write(
+  "<table width='300' style='border-collapse:collapse; text-align:center; border:2px solid white;'>"
+);
 
 // Table header with blue background, white text, and white border
-document.write("<tr style='background-color:blue; color:white; font-weight:bold; border:2px solid white;'>");
-document.write("<th style='border:2px solid white;'>SN</th>");
+document.write(
+  "<tr style='background-color:blue; color:white; font-weight:bold; border:2px solid white;'>"
+);
+document.write("<th style='border:2px solid white;'>S.N</th>");
 document.write("<th style='border:2px solid white;'>Name</th>");
 document.write("<th style='border:2px solid white;'>Image</th>");
-document.write("</tr>");
+document.write('</tr>');
 
 // Table rows with black text and white borders
 for (let i = 1; i <= 4; i++) {
-    let bgColor = i % 2 === 0 ? "#f2f2f2" : "#ffffff"; // Alternate row colors
-    document.write(`<tr style='background-color:${bgColor}; color:black; border:2px solid white;'>`);
-    document.write(`<td style='border:2px solid white;'>${i}</td>`);
-    document.write(`<td style='border:2px solid white;'>Name${i}</td>`);
-    document.write(`<td style='border:2px solid white;'>${i}.jpg</td>`);
-    document.write("</tr>");
+  let bgColor = i % 2 === 0 ? '#ffffff' : '#f2f2f2'; // Alternate row colors
+  document.write(
+    `<tr style='background-color:${bgColor}; color:black; border:2px solid white;'>`
+  );
+  document.write(`<td style='border:2px solid white;'>${i}</td>`);
+  document.write(`<td style='border:2px solid white;'>Name ${i}</td>`);
+  document.write(`<td style='border:2px solid white;'>${i}.jpg</td>`);
+  document.write('</tr>');
 }
 
-document.write("</table>");
+document.write('</table>');
