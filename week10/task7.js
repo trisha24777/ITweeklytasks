932d0f9150510a063a250fe2c17af4b943da6a67
Trisha Bhatta <trishabhatta14@gmail.com>
2025-04-03 22:16:27 +0545

    Update task7.js

--- week10/task7.js
+++ week10/task7.js
@@ -2,7 +2,7 @@ document.write("<table width='300' style='border-collapse:collapse; text-align:c
 
 // Table header with blue background, white text, and white border
 document.write("<tr style='background-color:blue; color:white; font-weight:bold; border:2px solid white;'>");
-document.write("<th style='border:2px solid white;'>SN</th>");
+document.write("<th style='border:2px solid white;'>S.N</th>");
 document.write("<th style='border:2px solid white;'>Name</th>");
 document.write("<th style='border:2px solid white;'>Image</th>");
 document.write("</tr>");
@@ -12,7 +12,7 @@ for (let i = 1; i <= 4; i++) {
     let bgColor = i % 2 === 0 ? "#f2f2f2" : "#ffffff"; // Alternate row colors
     document.write(`<tr style='background-color:${bgColor}; color:black; border:2px solid white;'>`);
     document.write(`<td style='border:2px solid white;'>${i}</td>`);
-    document.write(`<td style='border:2px solid white;'>Name${i}</td>`);
+    document.write(`<td style='border:2px solid white;'>Name ${i}</td>`);
     document.write(`<td style='border:2px solid white;'>${i}.jpg</td>`);
     document.write("</tr>");
 }
